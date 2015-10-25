0485995764310c312b1d2924260d5c58b73e408a
Frances <francesanderson3@gmail.com>
2015-10-25 13:32:17 +0200

    Transition from Login to GuardActivity in place. See TODO's for Manager

---
+++ AndroidStudioProjects/OnlineOBUI/app/src/main/java/za/astrosec/onlineobui/views/ManagerActivity.java
@@ -0,0 +1,22 @@
+package za.astrosec.onlineobui.views;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import za.astrosec.onlineobui.R;
+
+public class ManagerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager);
+
+    }
+
+}
